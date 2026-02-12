7eedf29d675f481d0cf076112b40038e67ee5fd9
Sirawat Wongmonta <birdy@airgradient.com>
2026-02-12 11:21:04 +0700

    feat: use NO2_PPB MeasurementType in main.

--- examples/OneOpenAir_AQMS600/OneOpenAir_AQMS600.ino
+++ examples/OneOpenAir_AQMS600/OneOpenAir_AQMS600.ino
@@ -1245,7 +1245,7 @@ static void updateTvoc(void) {
   measurements.update(Measurements::TVOCRaw, ag->sgp41.getTvocRaw());
   // measurements.update(Measurements::NOx, ag->sgp41.getNoxIndex());
   // measurements.update(Measurements::NOxRaw, ag->sgp41.getNoxRaw());
-  measurements.update(Measurements::NO2_PPB, (int)round(aqms600.get_no2()));
+  measurements.update(Measurements::NO2_PPB, aqms600.get_no2());
 }
 
 static void updatePMS5003() {
@@ -1507,6 +1507,7 @@ void setMeasurementMaxPeriod() {
   measurements.maxPeriod(Measurements::TVOCRaw, max);
   measurements.maxPeriod(Measurements::NOx, max);
   measurements.maxPeriod(Measurements::NOxRaw, max);
+  measurements.maxPeriod(Measurements::NO2_PPB, max);
 
   /// Max period for PMS sensors measurements
   max = calculateMaxPeriod(SENSOR_PM_UPDATE_INTERVAL);
