d41577ead428d3657fb03d17154c79b9e50ade34
Sirawat Wongmonta <birdy@airgradient.com>
2026-03-31 13:50:54 +0700

    feat: initial new source folder for new display prototype

---
+++ examples/OneOpenAir_NewDisplay/OpenMetrics.h
@@ -0,0 +1,29 @@
+#ifndef _OPEN_METRICS_H_
+#define _OPEN_METRICS_H_
+
+#include "AgConfigure.h"
+#include "AgValue.h"
+#include "AgWiFiConnector.h"
+#include "AirGradient.h"
+#include "Libraries/airgradient-client/src/airgradientClient.h"
+
+class OpenMetrics {
+private:
+  AirGradient *ag;
+  AirgradientClient *agClient;
+  Measurements &measure;
+  Configuration &config;
+  WifiConnector &wifiConnector;
+
+public:
+  OpenMetrics(Measurements &measure, Configuration &config,
+              WifiConnector &wifiConnector);
+  ~OpenMetrics();
+  void setAirGradient(AirGradient *ag);
+  void setAirgradientClient(AirgradientClient *client);
+  const char *getApiContentType(void);
+  const char* getApi(void);
+  String getPayload(void);
+};
+
+#endif /** _OPEN_METRICS_H_ */
